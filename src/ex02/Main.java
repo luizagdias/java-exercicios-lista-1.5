4d49719e8eb80b0d871d95ee9eb221e6be05064c
Luiza <luizagdias9@gmail.com>
2022-03-02 15:51:16 -0300

    exercicio 02

--- src/ex02/Main.java
+++ src/ex02/Main.java
@@ -1,5 +1,8 @@
 package ex02;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Main app = new Main();
@@ -7,8 +10,42 @@ public class Main {
     }
 
     private void start() {
-        
+        int i = 0;
+        float valor = 0;
+        ArrayList<Float> valores = new ArrayList();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor " + (i + 1) + " : ");
+        valor = scanner.nextFloat();
+
+        while (valor != -1) {
+            valores.add(i, valor);
+            i++;
+            System.out.print("Digite o valor " + (i + 1) + " : ");
+            valor = scanner.nextFloat();
+        }
+
+        if (valor == -1 && valores.size() < 4) {
+            System.out.println("Por favor, digite no mínimo 4 valores");
+            start();
+        } else {
+            calc(valores);
+        }
     }
 
+    private void calc(ArrayList<Float> valores) {
+        float soma = 0;
+        float media = 0;
 
+        for (float item : valores) {
+            soma = soma + item;
+        }
+
+        media = soma / valores.size();
+
+        System.out.println("-------------------------");
+        System.out.println("Soma: " + soma);
+        System.out.println("Média: " + media);
+    }
 }
